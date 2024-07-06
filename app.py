5832fea7e66f9d0896283aa1055f7ae902623c18
PRITHIV SAKTHI U R <117709109+PRITHIVSAKTHIUR@users.noreply.github.com>
2024-07-06 17:14:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import gradio as gr
 
-gr.load("------your model goes here---------").launch() #via huggingface spaces
+gr.load("------your model goes here---------").launch() #-------via huggingface spaces
 
 #(or) use it locally viia API. Visit ReadME.md
